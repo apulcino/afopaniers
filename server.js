082e03e97726ab18b332e18902a2ece613fffa6d
pulcino <alfredo.pulcino@afp.com>
2018-08-06 17:18:36 +0200

    Receive multicast messages

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const http = require('http');
 const application = require('./application');
 const constantes = require('../library/constantes');
-const port = process.env.PORT || 3002;
+const multicastRecver = require('../library/multicastRecver');
+const port = process.env.PORT || 0;
+
+let AFORegisteryUrl = [];
+
 const server = http.createServer(application);
 server.listen(port, function () {
   var host = constantes.getServerIpAddress();
   var port = server.address().port
   var intervalObj = setInterval(() => {
-    constantes.declareService(constantes.MSTypeEnum.afoPaniers, host, port, constantes.MSPathnameEnum.afoPaniers);
+    if (0 !== AFORegisteryUrl.length) {
+      constantes.declareService(AFORegisteryUrl, constantes.MSTypeEnum.afoPaniers, host, port, constantes.MSPathnameEnum.afoPaniers);
+    }
   }, 10000);
   console.log("SelectionsSrv listening at http://%s:%s", host, port)
 });
-console.log('RESTful API server started on: ' + port);
+
+const mcRecver = new multicastRecver(constantes.getServerIpAddress(), constantes.MCastAppPort, constantes.MCastAppAddr, (address, port, message) => {
+  console.log('SelectionsSrv : MCast Msg: From: ' + address + ':' + port + ' - ' + JSON.stringify(message));
+  var regUrl = 'http://' + message.host + ':' + message.port;
+  if (-1 === AFORegisteryUrl.indexOf(regUrl)) {
+    AFORegisteryUrl.push(regUrl);
+  }
+});
+
+console.log('SelectionsSrv RESTful API server started on: ' + port);
 
